915a9bfc44d25493145d0b544afbda19792721b4
nanocheeze <cybershrapnel@nanocheeze.com>
2019-04-06 03:08:23 -0700

    Add files via upload

---
+++ js/milestones.js
@@ -0,0 +1,123 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Gallery
+5. Init Milestones
+6. Init Logos Slider
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var ctrl = new ScrollMagic.Controller();
+
+
+	initMilestones();
+
+
+
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+
+	/* 
+
+	7. Init Gallery
+
+	*/
+
+
+	/* 
+
+	5. Init Milestones
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
+	/* 
+
+	6. Init Logos Slider
+
+	*/
+
+
+
+});
